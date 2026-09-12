89b98b3f6aa4107d16eb38dbefce5a517931ba4a
Ani Mazzucco <ciro040516@gmail.com>
2026-09-12 20:12:43 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//MENSAJE DE INICIO
+/*MENSAJE DE INICIO
 
 const respuesta = prompt("Bienvenidos a la página de nuestra comunidad. Nuestra región tiene paisajes muy bellos, cultivos diversos, y dintintos comercios y emprendimientos. A continuación le ofrecemos algunos contactos en caso de tener una emergencia. Coloque Aceptar para continuar o Cancelar para cerrar el mensaje.").toLowerCase();
 
@@ -15,7 +15,7 @@ if (respuesta === "aceptar") {
                     Para finalizar haga click en el botón de Aceptar`);
 } else {
     alert("Gracias por ingresar a nuestra página. Disfrute el contenido y ante cualquier consulta nuestros instituciones con sus contactos y localización en el mapa estan al final de la página. ¡Hasta luego!");
-}
+}*/
 
 
 
